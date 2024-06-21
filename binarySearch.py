bdf0fb700a2786f5ecb3b08b0eb8b05ebd24a33d
Daniel Lins <daniellins@MBP-de-Daniel.bbrouter>
2024-06-21 11:04:48 -0300

    selection sort

--- binarySearch.py
+++ binarySearch.py
@@ -3,7 +3,7 @@ def binary_search(list, item):
     high = len(list) - 1
     
     while low <= high: 
-        quite = (low + high) / 2
+        quite = (low + high) // 2
         kick = list[quite]
         if kick == item:
             return quite
@@ -15,8 +15,8 @@ def binary_search(list, item):
 
 my_list = [1, 3, 5, 7, 9]
 
-
-
+print(binary_search(my_list, 3))  # Uso correto de print em Python 3
+print(binary_search(my_list, -1))
 
     
     
